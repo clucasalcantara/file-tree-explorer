9fe45c0e6a588e6c3e7fd367c68d51f26602c637
Caio Alcantara <alcantaracaiolucas@gmail.com>
2023-06-16 15:35:07 -0700

    feat: create backend route to provide data

---
+++ src/pages/api/files.ts
@@ -0,0 +1,98 @@
+import type { DataNode } from "@/components/file-tree/types";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const data = {
+  name: "project",
+  kind: "directory",
+  children: [
+    {
+      name: "src",
+      kind: "directory",
+      children: [
+        {
+          name: "index.js",
+          kind: "file",
+          size: "1KB",
+          modified: "2022-03-08 11:30:00",
+        },
+        {
+          name: "components",
+          kind: "directory",
+          children: [
+            {
+              name: "Button.jsx",
+              kind: "file",
+              size: "2KB",
+              modified: "2022-03-07 15:00:00",
+            },
+            {
+              name: "Card.jsx",
+              kind: "file",
+              size: "3KB",
+              modified: "2022-03-06 10:00:00",
+            },
+          ],
+        },
+        {
+          name: "styles",
+          kind: "directory",
+          children: [
+            {
+              name: "index.css",
+              kind: "file",
+              size: "1KB",
+              modified: "2022-03-07 09:00:00",
+            },
+            {
+              name: "components.css",
+              kind: "file",
+              size: "2KB",
+              modified: "2022-03-06 12:00:00",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "public",
+      kind: "directory",
+      children: [
+        {
+          name: "index.html",
+          kind: "file",
+          size: "1KB",
+          modified: "2022-03-08 10:00:00",
+        },
+        {
+          name: "favicon.ico",
+          kind: "file",
+          size: "5KB",
+          modified: "2022-03-07 16:00:00",
+        },
+      ],
+    },
+    {
+      name: "package.json",
+      kind: "file",
+      size: "1KB",
+      modified: "2022-03-08 12:00:00",
+    },
+    {
+      name: "README.md",
+      kind: "file",
+      size: "2KB",
+      modified: "2022-03-08 13:00:00",
+    },
+  ],
+} as DataNode;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ files: DataNode }>
+) {
+  console.log("I was called");
+  console.log({ req });
+
+  return res.status(200).json({ files: data });
+}
